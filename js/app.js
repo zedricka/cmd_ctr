5486dbb504a4e74429640285d9be9379c962ca0e
zedricka <zedrick.ara@gmail.com>
2016-12-14 09:10:07 -1000

    wow

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function red () {
+  document.getElementById("box1").style.backgroundColor = "red";
+}
+
+function blue () {
+  document.getElementById("box2").style.backgroundColor = "blue";
+}
+
+function yellow () {
+  document.getElementById("box3").style.backgroundColor = "yellow";
+}
+
+function green () {
+  document.getElementById("box4").style.backgroundColor = "green";
+}
